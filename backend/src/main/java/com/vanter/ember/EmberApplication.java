5569f2a61cb3ae21b9adbb3de72c92534894c921
Fernand-O-band01 <112786231+Fernand-O-band01@users.noreply.github.com>
2026-05-10 18:46:55 -0600

    EMB-2: init spring project

---
+++ backend/src/main/java/com/vanter/ember/EmberApplication.java
@@ -0,0 +1,13 @@
+package com.vanter.ember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmberApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vanter/ember/EmberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanter.ember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
